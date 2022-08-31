83be6b772ec91f9db7591d982a3333f561211bb7
SirMeows <araelbrighteyes@gmail.com>
2022-08-31 19:02:09 +0200

    fixed vulnerability by adding input validation

--- nslookup.py
+++ nslookup.py
@@ -1,10 +1,24 @@
 import subprocess
+import re
 
 domain_name = input("Enter the domain name: ")
 
-command = "nslookup {}".format(domain_name)
+# string, from start to finish, must consist ONLY of the allowed characters
+# if any other character is found, None is returned
 
-response = subprocess.check_output(command, shell=True, encoding="UTF-8")
+pattern = '/^[a-zA-Z0-9\.]*$/'
 
-print(response)
+result = re.search(pattern, domain_name)
+
+if result == None:
+
+    print('illegal input you ruffian')
+
+else:
+
+    command = "nslookup {}".format(domain_name)
+
+    response = subprocess.check_output(command, shell=True, encoding="UTF-8")
+
+    print(response)
 
